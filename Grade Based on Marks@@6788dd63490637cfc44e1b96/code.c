8ef31aa5a0fa6c4cd02e8e8cd1a825e048cc1acc
AISHWIN <aishwinjindal907@gmail.com>
2025-03-27 18:55:54 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    if(gr>=90){printf("A");}
+    else if(gr>=80){printf("B");}
+    else if(gr>=70){printf("C");}
+    else if(gr>=60){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
+}
