5bd3cd8c71ab5effe6e6b66635bdbf6cbc4c16de
AISHWIN <aishwinjindal907@gmail.com>
2025-04-11 16:41:30 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    char ch;
+    scanf("%c", &ch);
+    if(ch=='A' ||ch=='B' || ch=='C'|| ch=='D'|| ch=='E'|| ch=='F'){
+        printf("Excellent");
+    }
+    else {
+        printf("Average");
+    }
+    }
